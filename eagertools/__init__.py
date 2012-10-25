264264a5ec27f90fc72d2d8af2998dd536e69096
Austin Bingham <austin.bingham@emerson.com>
2012-10-25 16:05:38 +0200

    fixed import of eager_map

--- eagertools/__init__.py
+++ eagertools/__init__.py
@@ -1,5 +1,5 @@
 # Hoist functions from their individual modules
-from map import map
+from eagertools.map import eager_map as map
 
 # Create some aliases in case people want to use the eager and lazy
 # versions at the same time.
