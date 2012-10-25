5122496784dee8ba8843f8f4d13d38a5e813b327
Austin Bingham <austin.bingham@emerson.com>
2012-10-25 15:45:55 +0200

    Added some setup stuff.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'eagertools',
+    version = '0.1',
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = 'Austin Bingham',
+    author_email = 'austin.bingham@gmail.com',
+    description = 'An eager version of itertools.',
+    license = 'MIT',
+    keywords = '',
+    url = 'http://github.com/abingham/eagertools',
+)
