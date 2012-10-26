d6e2126dcceb1e2b8555b34223c0a011b374679b
Austin Bingham <austin.bingham@emerson.com>
2012-10-26 08:25:16 +0200

    added some tests.

---
+++ eagertools/test/map_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from eagertools import map
+
+def no_args():
+    return 1
+
+def one_arg(x):
+    return x
+
+def two_args(x, y):
+    return (x,y)
+
+class Test(unittest.TestCase):
+    def test_no_args(self):
+        x = map(no_args, [])
+        self.assertEqual(x, [])
+
+    def test_one_iter(self):
+        x = map(one_arg, range(100))
+        self.assertEqual(x, list(range(100)))
+
+    def test_two_iters(self):
+        x = map(two_args, range(100), range(100, 200))
+        self.assertEqual(x, list(zip(range(100), range(100, 200))))
