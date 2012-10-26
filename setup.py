a87a36b8f119d8c1ba6fc1e534f4e6bca3bc9f0f
Austin Bingham <austin.bingham@emerson.com>
2012-10-26 14:57:55 +0200

    Fixed up setup.py a bit.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import sys
-
 import distribute_setup
 distribute_setup.use_setuptools()
 
@@ -7,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'eagertools',
-    version = '0.1',
+    version = '0.2',
     packages = find_packages(),
 
     # metadata for upload to PyPI
@@ -15,6 +13,20 @@ setup(
     author_email = 'austin.bingham@gmail.com',
     description = 'Eager version of some lazy functions.',
     license = 'MIT',
-    keywords = '',
+    keywords = 'functional',
     url = 'http://github.com/abingham/eagertools',
+    downloadurl = 'http://pypi.python.org/pypi/eagertools',
+    # long_description
+    # zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+        ],
+    platforms='any',
+    include_package_data=True,
 )
