5c90c009fe5df6f715dbb06f6cc7941f15deb105
Austin Bingham <austin.bingham@gmail.com>
2018-07-11 11:47:55 +0200

    Modernizing things a bit.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'eagertools',
-    version = '0.4',
-    packages = find_packages(),
+    name='eagertools',
+    version='0.4.1',
+    packages=find_packages(),
 
     # metadata for upload to PyPI
-    author = 'Austin Bingham',
-    author_email = 'austin.bingham@gmail.com',
-    description = 'Eager version of some lazy functions.',
-    license = 'MIT',
-    keywords = 'functional',
-    url = 'http://github.com/abingham/eagertools',
-    downloadurl = 'http://pypi.python.org/pypi/eagertools',
+    author='Austin Bingham',
+    author_email='austin.bingham@gmail.com',
+    description='Eager version of some lazy functions.',
+    license='MIT',
+    keywords='functional',
+    url='http://github.com/abingham/eagertools',
+    downloadurl='http://pypi.python.org/pypi/eagertools',
     # long_description
     # zip_safe=False,
     classifiers=[
@@ -23,7 +23,16 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries'
-        ],
+    ],
     platforms='any',
     include_package_data=True,
+    install_requires=[
+        'more-itertools',
+    ],
+    extras_require={
+        # 'dev': ['check-manifest', 'wheel'],
+        # 'doc': ['sphinx', 'cartouche'],
+        'test': ['pytest', 'tox'],
+    },
+
 )
